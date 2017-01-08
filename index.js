2290056d065874b7be237887a55d601b0867d716
Andre Milton <wredhyn@gmail.com>
2017-01-08 04:19:36 +0000

    POC: using timed events for socket based movement

--- index.js
+++ index.js
@@ -10,9 +10,51 @@ http.listen(80, function(){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.emit('time', getTime());
+  // console.log(io.sockets.connected);
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    // to do
   });
 });
+
+function getTime() {
+  var timeTupple = process.hrtime(startTime);
+  return timeTupple[0] * 1e3 + timeTupple[1] / 1e6;
+}
+
+function getPositionAtTime(moveCommand, time) {
+  return [
+    moveCommand.position[0] + moveCommand.velocity * Math.cos(moveCommand.direction) * time,
+    moveCommand.position[1] + moveCommand.velocity * Math.sin(moveCommand.direction) * time
+  ];
+}
+
+var tickCount = 0,
+    turnEvery = 2000; // ms
+
+var startTime = process.hrtime();
+var units = {
+  "0": {
+    moveCommand: {
+      direction: 0, // rads
+      velocity: 0.1,
+      position: [0,0],
+      timeStamp: 0
+    }
+  } // this should be uuencoded
+};
+
+function gameLoop() {
+  var timeStamp = getTime();
+  if (timeStamp - units["0"].moveCommand.timeStamp > turnEvery) {
+    units["0"].moveCommand.position = getPositionAtTime(units["0"].moveCommand, timeStamp - units["0"].moveCommand.timeStamp);
+    // change direction and take a new timestamp
+    units["0"].moveCommand.direction += Math.PI / 2;
+    units["0"].moveCommand.timeStamp = timeStamp;
+    io.emit('command', units);
+  }
+  setImmediate(gameLoop);
+}
+// start
+gameLoop();
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,57 @@
 "use strict";
 
-var socket = io();
+function Main() {
+	var units = {},
+			canvas,
+			context,
+			socket,
+			timeStart = 0; // ms - reset from server
 
-class Main {
-	constructor (dom) {
-		this.dom = dom;
-		this.dom.innerText = "Works";
-	}
+		function setup() {
+			canvas = document.createElement("canvas"),
+			canvas.width = 1000;
+			canvas.height = 1000;
+			document.body.appendChild(canvas);
+			context = canvas.getContext("2d");
+			context.fillStyle = "white";
+
+			socket = io();
+			socket.on('time', function (data) {
+				// current time server side (without counting latency)
+				timeStart = data - performance.now();
+				// start
+				gameLoop();
+			});
+			socket.on('command', function (data) {
+				units = data;
+			});
+		}
+
+		function getPositionAtTime(position, velocity, direction, time) {
+		  return [
+		    position[0] + velocity * Math.cos(direction) * time,
+		    position[1] + velocity * Math.sin(direction) * time
+		  ];
+		}
+
+
+		function gameLoop() {
+			// clear canvas
+			context.clearRect(0, 0, canvas.width, canvas.height);
+
+			var time = performance.now() + timeStart;
+
+			// loop through elements
+			for (var unit in units) {
+				var unit = units[unit];
+				var pos = getPositionAtTime(unit.moveCommand.position, unit.moveCommand.velocity, unit.moveCommand.direction, time - unit.moveCommand.timeStamp);
+				context.fillRect(Math.floor(canvas.width / 2 + pos[0]), Math.floor(canvas.height / 2 + pos[1]), 2, 2);
+			}
+
+			requestAnimationFrame(gameLoop)
+		}
+
+		setup();
 }
 
-window.addEventListener('load', function (e) {
-	var main = new Main(document.getElementById('game'));
-});
+window.addEventListener('load', Main);
